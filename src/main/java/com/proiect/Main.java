6b51705bc74c565805da341384147d5d182d8524
Madalina-Iulia Rusu <rusumadalina16@gmail.com>
2021-09-22 17:34:53 +0300

    initial

---
+++ src/main/java/com/proiect/Main.java
@@ -0,0 +1,23 @@
+package com.proiect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.validation.annotation.Validated;
+
+import java.util.TimeZone;
+
+@SpringBootApplication
+@Validated
+class Main extends SpringBootServletInitializer {
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(Main.class);
+    }
+
+    public static void main(String[] args) {
+        TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
+        SpringApplication.run(Main.class, args);
+    }
+}
